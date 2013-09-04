daaed56d498654f0c705de60ce80445a80643059
Rosario Di Somma <rosario.disomma@dreamhost.com>
2013-09-04 23:29:02 +0200

    Add test for the USR1 signal handler
    DHC-1410
    
    Test the USR1 signal handler and the Worker.handle_message
    method
    
    Change-Id: I504737334f7edf459ff4c0dace35dfbcc82b0dd7
    Signed-off-by: Rosario Di Somma <rosario.disomma@dreamhost.com>

--- akanda/rug/test/unit/test_main.py
+++ akanda/rug/test/unit/test_main.py
@@ -1,4 +1,5 @@
 import mock
+import signal
 import unittest2 as unittest
 
 from akanda.rug import main
@@ -16,3 +17,17 @@ class TestMain(unittest.TestCase):
         main.shuffle_notifications(queue, sched)
         sched.handle_message.assert_called_once('message')
         sched.stop.assert_called_once()
+
+    @mock.patch('akanda.rug.main.cfg')
+    @mock.patch('akanda.rug.main.multiprocessing')
+    @mock.patch('akanda.rug.main.notifications')
+    @mock.patch('akanda.rug.main.scheduler')
+    @mock.patch('akanda.rug.main.populate')
+    @mock.patch('akanda.rug.main.health')
+    @mock.patch('akanda.rug.main.shuffle_notifications')
+    @mock.patch('akanda.rug.main.signal.signal')
+    def test_sigusr1_handler(self, mock_signal, shuffle_notifications, health,
+                             populate, scheduler, notifications,
+                             multiprocessing, cfg):
+        main.main()
+        mock_signal.assert_called_once_with(signal.SIGUSR1, mock.ANY)
