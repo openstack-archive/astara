6802161df067469e54101dffb26cd6e19a7f25af
Doug Hellmann <doug.hellmann@dreamhost.com>
2014-01-28 08:41:32 -0800

    Add comments about commands
    Try to provide more detail about the commands without
    going too far into creating factories and classes
    to represent them, yet.
    
    Change-Id: I58359dea069fea57b0106f1d46ff77522b4a8405

--- akanda/rug/commands.py
+++ akanda/rug/commands.py
@@ -1,10 +1,15 @@
 """Constants for the commands
 """
 
+# Dump debugging details about the worker processes and threads
 WORKERS_DEBUG = 'workers-debug'
 
+# Put a router in debug/manage mode
+# Expects a 'router_id' argument in the payload with the UUID of the router
 ROUTER_DEBUG = 'router-debug'
 ROUTER_MANAGE = 'router-manage'
 
+# Put a tenant in debug/manage mode
+# Expects a 'tenant_id' argument in the payload with the UUID of the tenant
 TENANT_DEBUG = 'tenant-debug'
 TENANT_MANAGE = 'tenant-manage'
