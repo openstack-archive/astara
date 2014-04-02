8037acb2f627ab9cc732fc813f5a3e28e67f7477
Doug Hellmann <doug.hellmann@dreamhost.com>
2014-04-02 15:01:00 -0700

    Set the router-id for delete events
    Change-Id: I65a4302e50c2fccff101b35689cdf43ccda5c63b

--- akanda/rug/notifications.py
+++ akanda/rug/notifications.py
@@ -74,6 +74,7 @@ def _make_event_from_message(message):
             crud = event.CREATE
         elif event_type == 'router.delete.end':
             crud = event.DELETE
+            router_id = message.get('payload', {}).get('router_id')
         elif event_type in _INTERESTING_NOTIFICATIONS:
             crud = event.UPDATE
         elif event_type.endswith('.end'):
