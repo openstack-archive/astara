7456983a4f2de0c5338f278f31aa70eb6ed3d6b1
Jonathan LaCour <jonathan@cleverdevil.org>
2014-05-27 14:07:14 -0700

    Cleanup

--- akanda/rug/common/__init__.py
+++ akanda/rug/common/__init__.py
@@ -1,6 +1,6 @@
-# Copyright 2014 DreamHost, LLC 
+# Copyright 2014 DreamHost, LLC
 #
-# Author: DreamHost, LLC 
+# Author: DreamHost, LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may
 # not use this file except in compliance with the License. You may obtain
@@ -13,5 +13,3 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
 # under the License.
-
-
--- akanda/rug/openstack/common/notifier/__init__.py
+++ akanda/rug/openstack/common/notifier/__init__.py
@@ -1,6 +1,6 @@
-# Copyright 2014 DreamHost, LLC 
+# Copyright 2014 DreamHost, LLC
 #
-# Author: DreamHost, LLC 
+# Author: DreamHost, LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may
 # not use this file except in compliance with the License. You may obtain
