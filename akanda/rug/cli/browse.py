b41e5aa083678e635776de55c0e028e46b58b6a0
Ryan Petrello <lists@ryanpetrello.com>
2014-07-10 13:36:49 -0700

    Add multiprocessing to API calls to improve `rug-ctl browse` performance.

--- akanda/rug/cli/browse.py
+++ akanda/rug/cli/browse.py
@@ -18,6 +18,10 @@
 """Interactive CLI for rebuilding routers
 """
 import logging
+import multiprocessing
+import time
+import sqlite3
+import tempfile
 
 from akanda.rug import commands
 from akanda.rug.api import nova as nova_api
@@ -27,26 +31,113 @@ from blessed import Terminal
 from oslo.config import cfg
 
 
+class ConfigSub(object):
+
+    def __init__(self, admin_user, admin_password, tenant_name, auth_url,
+                 auth_strategy, auth_region):
+        self.admin_user = admin_user
+        self.admin_password = admin_password
+        self.tenant_name = tenant_name
+        self.admin_tenant_name = tenant_name
+        self.auth_url = auth_url
+        self.auth_strategy = auth_strategy
+        self.auth_region = auth_region
+
+
+class RouterRow(object):
+
+    def __init__(self, *args):
+        self.id, self.name, self.status, self.latest, self.image_name = args
+        self.tenant_id = self.name.replace('ak-', '')
+        self.image_name = self.image_name or '<no vm>'
+
+
+class RouterFetcher(object):
+
+    def __init__(self, conf, db):
+        self.conn = sqlite3.connect(db)
+        self.nova = nova_api.Nova(conf)
+        self.quantum = quantum_api.Quantum(conf)
+
+    def fetch(self):
+        routers = self.quantum.get_routers()
+        c = self.conn.cursor()
+        for router in routers:
+            instance = None
+            image = None
+            try:
+                instance = self.nova.get_instance(router)
+            except:
+                pass
+            if instance and instance.image:
+                image = self.nova.client.images.get(instance.image['id'])
+            sql = ''.join([
+                "INSERT OR REPLACE INTO routers ",
+                "('id', 'name', 'status', 'latest', 'image_name') VALUES (",
+                ', '.join("?" * 5),
+                ");"
+            ])
+            c.execute(sql, (
+                router.id,
+                router.name,
+                router.status,
+                image and image.id == cfg.CONF.router_image_uuid,
+                image.name if image else ''
+            ))
+            self.conn.commit()
+
+
+def populate_routers(db, *args):
+    conf = ConfigSub(*args)
+    client = RouterFetcher(conf, db)
+    while True:
+        client.fetch()
+        time.sleep(.1)
+
+
 class BrowseRouters(message.MessageSending):
 
     log = logging.getLogger(__name__)
+    SCHEMA = '''CREATE TABLE routers (
+        id TEXT PRIMARY KEY,
+        name TEXT,
+        status TEXT,
+        latest INTEGER,
+        image_name TEXT
+    );'''
 
     def __init__(self, *a, **kw):
         self.term = Terminal()
-        self.nova = nova_api.Nova(cfg.CONF)
-        self.quantum = quantum_api.Quantum(cfg.CONF)
         self.position = 0
-        self.image_cache = {}
+        self.init_database()
+        p = multiprocessing.Process(
+            target=populate_routers,
+            args=(
+                self.fh.name,
+                cfg.CONF.admin_user,
+                cfg.CONF.admin_password,
+                cfg.CONF.admin_tenant_name,
+                cfg.CONF.auth_url,
+                cfg.CONF.auth_strategy,
+                cfg.CONF.auth_region
+            )
+        )
+        p.start()
         super(BrowseRouters, self).__init__(*a, **kw)
+
+    def init_database(self):
+        self.fh = tempfile.NamedTemporaryFile()
+        self.conn = sqlite3.connect(self.fh.name)
+        c = self.conn.cursor()
+        c.execute(self.SCHEMA)
+        self.conn.commit()
 
     def take_action(self, parsed_args):
         with self.term.fullscreen():
             with self.term.cbreak():
                 val = None
                 while val != u'q':
-                    if not val:
-                        self.fetch_routers()
-                    elif val.is_sequence:
+                    if val and val.is_sequence:
                         if val.code == self.term.KEY_DOWN:
                             self.move_down()
                         if val.code == self.term.KEY_UP:
@@ -58,52 +149,45 @@ class BrowseRouters(message.MessageSending):
                     elif val == u'r':
                         self.rebuild_router()
                     self.print_routers()
-                    val = self.term.inkey(timeout=3)
+                    val = self.term.inkey(timeout=1)
 
-    def fetch_routers(self):
-        self.routers = self.quantum.get_routers()
+    def clean_up(self, cmd, result, err):
+        self.fh.close()
+        return super(BrowseRouters).clean_up(cmd, result, err)
+
+    @property
+    def routers(self):
+        c = self.conn.cursor()
+        c.execute('SELECT * FROM routers ORDER BY id ASC;')
+        return [RouterRow(*row) for row in c.fetchall()]
 
     @property
     def window(self):
         offset = 0
+        routers = self.routers
         visible_height = self.term.height - 2
-        if len(self.routers) > visible_height:
+        if len(routers) > visible_height:
             offset = self.position
-            offset = min(offset, len(self.routers) - visible_height - 1)
-        routers = self.routers[offset:(offset+visible_height+1)]
-        for router in routers:
-            if router.id not in self.image_cache:
-                try:
-                    instance = self.nova.get_instance(router)
-                except:
-                    instance = None
-                if instance and instance.image:
-                    image = self.nova.client.images.get(instance.image['id'])
-                    status = self.term.red('OUT OF DATE')
-                    if image.id == cfg.CONF.router_image_uuid:
-                        status = self.term.green('LATEST')
-                    name = status.ljust(11) + ' ' + image.name
-                    self.image_cache[router.id] = name
-                else:
-                    self.image_cache[router.id] = '<no vm>'
-            setattr(router, 'image', self.image_cache[router.id])
-
-        return offset, routers
+            offset = min(offset, len(routers) - visible_height - 1)
+        return offset, routers[offset:(offset+visible_height+1)]
 
     def print_routers(self):
         offset, routers = self.window
         with self.term.location():
             for i, r in enumerate(routers):
-                formatter = lambda x: x
+                age = self.term.red('OUT-OF-DATE')
+                if r.latest:
+                    age = self.term.green('LATEST'.ljust(11))
                 args = [
                     r.id,
                     r.name,
                     self.router_states[r.status](r.status.ljust(7)),
-                    r.image
+                    age,
+                    r.image_name
                 ]
                 if i + offset == self.position:
-                    formatter = self.term.reverse
-                print self.term.move(i, 0) + formatter(' '.join(args)).ljust(
+                    args = map(self.term.reverse, args)
+                print self.term.move(i, 0) + ' '.join(args).ljust(
                     self.term.width
                 )
 
@@ -115,10 +199,11 @@ class BrowseRouters(message.MessageSending):
         }
 
     def rebuild_router(self):
-        router = self.routers[self.position]
-        router.status = 'REBUILD'
-        del self.image_cache[router.id]
-        self.send_message(self.make_message(router))
+        r = self.routers[self.position]
+        c = self.conn.cursor()
+        c.execute('UPDATE routers SET status=? WHERE id=?', ('REBUILD', r.id))
+        self.conn.commit()
+        self.send_message(self.make_message(r))
 
     def move_up(self):
         self.position = max(0, self.position-1)
