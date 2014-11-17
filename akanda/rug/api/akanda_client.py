f9e89ede80843d8f45a8139cb3aa11f849673276
Jeremy Hanmer <jeremy@fishbiscuits.org>
2014-11-17 10:47:13 -0800

    add debugging output to is_alive check

--- akanda/rug/api/akanda_client.py
+++ akanda/rug/api/akanda_client.py
@@ -15,6 +15,7 @@
 # under the License.
 
 
+import logging
 import requests
 
 from oslo.config import cfg
@@ -24,6 +25,7 @@ AKANDA_ULA_PREFIX = 'fdca:3ba5:a17a:acda::/64'
 AKANDA_MGT_SERVICE_PORT = 5000
 AKANDA_BASE_PATH = '/v1/'
 
+LOG = logging.getLogger(__name__)
 
 def _mgt_url(host, port, path):
     if ':' in host:
@@ -45,8 +47,8 @@ def is_alive(host, port):
         r = s.get(_mgt_url(host, port, path), timeout=cfg.CONF.alive_timeout)
         if r.status_code == 200:
             return True
-    except:
-        pass
+    except Exception as e:
+        LOG.debug('is_alive for %s failed: %s', host, str(e))
     return False
 
 
