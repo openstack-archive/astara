61ae732eb95c758e6f35b72d30baba8e6b5c03d0
Adam Gandelman <adamg@ubuntu.com>
2015-05-05 17:05:37 -0700

    Update unittests for Kilo
    This updates unit tests to pass with recent Kilo refactoring and
    enhancements.  A few irrelevant tests were removed, specifically
    those that tested a specific state where routers have no management
    port, which is no longer a possible state.
    
    Change-Id: I2e06a0e5416f361c6e6caf55971c931ac00b9bf5

--- akanda/rug/vm_manager.py
+++ akanda/rug/vm_manager.py
@@ -311,6 +311,7 @@ class VmManager(object):
             return
 
         # FIXME: This should raise an explicit exception so the caller
+
         # knows that we could not talk to the router (versus the issue
         # above).
         interfaces = router_api.get_interfaces(
@@ -404,7 +405,6 @@ class VmManager(object):
 
         logical_networks = set(p.network_id for p in self.router_obj.ports)
 
-        ports_to_delete = []
         if logical_networks != instance_networks:
             instance = worker_context.nova_client.get_instance_by_id(
                 self.instance_info.id_
@@ -434,7 +434,6 @@ class VmManager(object):
                 instance.interface_detach(port.id)
 
                 self.instance_info.ports.remove(port)
-                ports_to_delete.append(port)
 
         # The action of attaching/detaching interfaces in Nova happens via the
         # message bus and is *not* blocking.  We need to wait a few seconds to
