a255bc9637e0c3e98665a6ac18fc277010269ea4
Adam Gandelman <adamg@ubuntu.com>
2015-06-08 13:32:05 -0700

    Wait for a configured test router to become active
    The router VM that is spawned during RUG startup may not be active until some
    time after the test suite, causing tests to start before the thing is active.
    This adds an assertion to the functional test setUp to wait for a configured
    router to become active before continuing on with any tests.  The router to
    wait on is now configured in test.conf.  run_functional.sh will attempt to find
    the devstack created one, and error if there are multiples.  Users can specify
    their own test router if running outside of the gate env.
    
    Change-Id: I2b54ddfcce8dab446ac7418cca61320b370bc999

--- tools/run_functional.sh
+++ tools/run_functional.sh
@@ -9,8 +9,28 @@ if [ -z "$SERVICE_TENANT_ID" ]; then
     SERVICE_TENANT_ID="$(keystone tenant-list | grep $SERVICE_TENANT_NAME | awk '{ print $2 }')"
 fi
 
+# Functional tests require a test akanda router be created prior to the test
+# run. Devstack does this, but you may specify another here.  If not specified,
+# the ID of the devstack created router will be used.
+AKANDA_TEST_ROUTER_UUID=${AKANDA_TEST_ROUTER_UUID:-''}
+
+function find_router() {
+    # Find the UUID of the akanda router created by devstack.
+    router=$(neutron router-list | grep "ak-" | awk '{ print $2 }')
+    if [ $(echo "$router" | wc -l) -gt 1 ]; then
+        echo "ERROR: Found multiple akanda routers, cannot continue."
+        exit 1
+    elif [ -z "$router" ]; then
+        echo "ERROR: Could not locate akanda router."
+        exit 1
+    fi
+    echo $router
+}
+
+
 cat <<END >$CONFIG_FILE
 [functional]
+appliance_active_timeout=240
 os_auth_url=$OS_AUTH_URL
 os_username=$OS_USERNAME
 os_password=$OS_PASSWORD
@@ -19,5 +39,10 @@ service_tenant_name=$SERVICE_TENANT_NAME
 service_tenant_id=$SERVICE_TENANT_ID
 appliance_api_port=$APPLIANCE_API_PORT
 END
+
+if [ -z "$AKANDA_TEST_ROUTER_UUID" ]; then
+    AKANDA_TEST_ROUTER_UUID="$(find_router)"
+fi
+echo "akanda_test_router_uuid=$AKANDA_TEST_ROUTER_UUID" >>$CONFIG_FILE
 
 sudo -E tox -e  functional
