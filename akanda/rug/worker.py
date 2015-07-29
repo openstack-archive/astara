29a7a802b5b7a1d80bae5441d012e9983412478e
Adam Gandelman <adamg@ubuntu.com>
2015-07-28 21:58:00 -0700

    Adds a DB layer, use it for debug modes
    In preparation for scale out RUG, this adds a database layer
    (built on oslo.db) that will be used for managing debug modes.
    Instead of tracking debug'd/ignored routers and tenants in-memory
    or on disk as files, this uses a database. This means that putting
    things into debug mode via rug-ctl are now persistent, and the file-based
    approach is no longer available.  A sqlite database (the default) can be
    used for single node installs, or the RUG can be pointed at mysql/pg to
    handle this in larger environments.
    
    This also adds a global debug mode that can be used to ignore all events
    during maintanence periods.
    
    A new optional 'reason' argument has been added to the debug modes, allowing
    operators to add a note when entering a tenant/router/cluster into debug
    mode.
    
    Change-Id: I3f5129e11b11cf5aaed8889da3b204104e5ad203
    Closes-bug: #1470619
    Partially implements: blueprint rug-scaling

--- akanda/rug/worker.py
+++ akanda/rug/worker.py
@@ -19,7 +19,6 @@
 """
 
 import collections
-import os
 import Queue
 import threading
 import uuid
@@ -34,6 +33,7 @@ from akanda.rug import event
 from akanda.rug import tenant
 from akanda.rug.api import nova
 from akanda.rug.api import neutron
+from akanda.rug.db import api as db_api
 
 LOG = logging.getLogger(__name__)
 CONF = cfg.CONF
@@ -99,9 +99,10 @@ class Worker(object):
         # The notifier needs to be started here to ensure that it
         # happens inside the worker process and not the parent.
         self.notifier.start()
-        # Track the routers and tenants we are told to ignore
-        self._debug_routers = set()
-        self._debug_tenants = set()
+
+        # The DB is used for trakcing debug modes
+        self.db_api = db_api.get_instance()
+
         # Thread locks for the routers so we only put one copy in the
         # work queue at a time
         self._router_locks = collections.defaultdict(threading.Lock)
@@ -142,11 +143,13 @@ class Worker(object):
             if sm is None:
                 LOG.info('received stop message')
                 break
+
             # Make sure we didn't already have some updates under way
             # for a router we've been told to ignore for debug mode.
-            if sm.router_id in self._debug_routers:
-                LOG.debug('skipping update of router %s (in debug mode)',
-                          sm.router_id)
+            should_ignore, reason = self.db_api.router_in_debug(sm.router_id)
+            if should_ignore:
+                LOG.debug('skipping update of router %s in debug mode ( '
+                          'reason: ', sm.router_id, reason)
                 continue
             # FIXME(dhellmann): Need to look at the router to see if
             # it belongs to a tenant which is in debug mode, but we
@@ -233,6 +236,23 @@ class Worker(object):
             )
         return [self.tenant_managers[tenant_id]]
 
+    def _should_process(self, message):
+        """Determines whether a message should be processed or not."""
+        global_debug, reason = self.db_api.global_debug()
+        if global_debug:
+            LOG.info('Skipping incoming event, cluster in global debug '
+                     'mode. (reason: %s)' % reason)
+            return False
+        should_ignore, reason = self.db_api.tenant_in_debug(message.tenant_id)
+        if should_ignore:
+            LOG.info(
+                'Ignoring message intended for tenant %s in debug mode '
+                '(reason: %s): %s',
+                message.tenant_id, reason, message,
+            )
+            return False
+        return message
+
     def handle_message(self, target, message):
         """Callback to be used in main
         """
@@ -244,6 +264,8 @@ class Worker(object):
         if message.crud == event.COMMAND:
             self._dispatch_command(target, message)
         else:
+            if not self._should_process(message):
+                return
             # This is an update command for the router, so deliver it
             # to the state machine.
             with self.lock:
@@ -261,18 +283,20 @@ class Worker(object):
 
         elif instructions['command'] == commands.ROUTER_DEBUG:
             router_id = instructions['router_id']
+            reason = instructions.get('reason')
             if router_id in commands.WILDCARDS:
                 LOG.warning(
                     'Ignoring instruction to debug all routers with %r',
                     router_id)
             else:
-                LOG.info('Placing router %s in debug mode', router_id)
-                self._debug_routers.add(router_id)
+                LOG.info('Placing router %s in debug mode (reason: %s)',
+                         router_id, reason)
+                self.db_api.enable_router_debug(router_id, reason)
 
         elif instructions['command'] == commands.ROUTER_MANAGE:
             router_id = instructions['router_id']
             try:
-                self._debug_routers.remove(router_id)
+                self.db_api.disable_router_debug(router_id)
                 LOG.info('Resuming management of router %s', router_id)
             except KeyError:
                 pass
@@ -301,22 +325,35 @@ class Worker(object):
 
         elif instructions['command'] == commands.TENANT_DEBUG:
             tenant_id = instructions['tenant_id']
+            reason = instructions.get('reason')
             if tenant_id in commands.WILDCARDS:
                 LOG.warning(
                     'Ignoring instruction to debug all tenants with %r',
                     tenant_id)
             else:
-                LOG.info('Placing tenant %s in debug mode', tenant_id)
-                self._debug_tenants.add(tenant_id)
+                LOG.info('Placing tenant %s in debug mode (reason: %s)',
+                         tenant_id, reason)
+                self.db_api.enable_tenant_debug(tenant_id, reason)
 
         elif instructions['command'] == commands.TENANT_MANAGE:
             tenant_id = instructions['tenant_id']
             try:
-                self._debug_tenants.remove(tenant_id)
+                self.db_api.disable_tenant_debug(tenant_id)
                 LOG.info('Resuming management of tenant %s', tenant_id)
             except KeyError:
                 pass
 
+        elif instructions['command'] == commands.GLOBAL_DEBUG:
+            enable = instructions.get('enabled')
+            reason = instructions.get('reason')
+            if enable == 1:
+                LOG.info('Enabling global debug mode (reason: %s)' % reason)
+                self.db_api.enable_global_debug(reason)
+            elif enable == 0:
+                LOG.info('Disabling global debug mode')
+                self.db_api.disable_global_debug()
+            else:
+                LOG.warn('Unrecognized global debug command: %s', instructions)
         elif instructions['command'] == commands.CONFIG_RELOAD:
             try:
                 cfg.CONF()
@@ -328,34 +365,22 @@ class Worker(object):
         else:
             LOG.warn('unrecognized command: %s', instructions)
 
-    def _get_routers_to_ignore(self):
-        ignores = set()
-        try:
-            if self._ignore_directory:
-                ignores = set(os.listdir(self._ignore_directory))
-        except OSError:
-            pass
-        return ignores
-
     def _deliver_message(self, target, message):
-        if target in self._debug_tenants:
-            LOG.info(
-                'Ignoring message intended for tenant %s: %s',
-                target, message,
-            )
-            return
         LOG.debug('preparing to deliver %r to %r', message, target)
-        routers_to_ignore = self._debug_routers.union(
-            self._get_routers_to_ignore()
-        )
         trms = self._get_trms(target)
         for trm in trms:
             sms = trm.get_state_machines(message, self._context)
             for sm in sms:
-                if sm.router_id in routers_to_ignore:
+                # NOTE(adam_g): We dont necessarily know the router_id
+                # till the sm has been created. this check should move to
+                # _should_process() once thats changed.
+                should_ignore, reason = self.db_api.router_in_debug(
+                    sm.router_id)
+                if should_ignore:
                     LOG.info(
-                        'Ignoring message intended for %s: %s',
-                        sm.router_id, message,
+                        'Ignoring message intended for router %s in '
+                        'debug mode (reason: %s): %s',
+                        sm.router_id, reason, message,
                     )
                     continue
                 # Add the message to the state machine's inbox. If
@@ -402,16 +427,16 @@ class Worker(object):
                 'alive' if thread.isAlive() else 'DEAD',
                 self._thread_status.get(thread.name, 'UNKNOWN'),
             )
-        for tid in sorted(self._debug_tenants):
-            LOG.info('Debugging tenant: %s', tid)
-        if not self._debug_tenants:
+        debug_tenants = self.db_api.tenants_in_debug()
+        if debug_tenants:
+            for t_uuid, reason in debug_tenants:
+                LOG.info('Debugging tenant: %s (reason: %s)', t_uuid, reason)
+        else:
             LOG.info('No tenants in debug mode')
-        for rid in sorted(self._debug_routers):
-            LOG.info('Debugging router: %s', rid)
-        if not self._debug_routers:
+
+        debug_routers = self.db_api.routers_in_debug()
+        if self.db_api.routers_in_debug():
+            for r_uuid, reason in debug_routers:
+                LOG.info('Debugging router: %s (reason: %s)', r_uuid, reason)
+        else:
             LOG.info('No routers in debug mode')
-        ignored_routers = sorted(self._get_routers_to_ignore())
-        for rid in ignored_routers:
-            LOG.info('Ignoring router: %s', rid)
-        if not ignored_routers:
-            LOG.info('No routers being ignored')
