0c6ef140303bce12aa805cd22e2613968953be67
Rama Puranam <rama.puranam@gmail.com>
2015-08-06 15:33:58 -0700

    Adds docstring for akanda.rug.daemon.ignore_signals()
    Inclusion of docstring as part of the method implementation for
    better readability and maintainability.
    
    Closes-bug: #1481458
    
    Partially Implements: blueprint liberty-doc-udpates
    
    Change-Id: I1acb452764de449154959787bf423af3d9fa01b5

--- akanda/rug/daemon.py
+++ akanda/rug/daemon.py
@@ -24,7 +24,20 @@ from oslo_log import log as logging
 
 
 def ignore_signals():
-    """Ignore signals that might interrupt processing.
+    """Ignore signals that might interrupt processing
+
+    Since the RUG doesn't want to be asynchronousyly interrupted,
+    various signals received needs to be ignored. The registered
+    signals including SIGHUP, SIGALRM, and default signals
+    SIGUSR1 and SIGUSR2 are captured and ignored through the SIG_IGN
+    action.
+
+    :param: None
+
+    :returns: None
+
+    :raises: None
+
     """
     for s in [signal.SIGHUP, signal.SIGUSR1, signal.SIGUSR2, signal.SIGALRM]:
         logging.getLogger(__name__).info('ignoring signal %s', s)
