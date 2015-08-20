c4bf444f9c2c8c8d1e77b144b4a82449048c481a
David Lenwell <dlenwell@gmail.com>
2015-08-20 09:26:38 -0700

    Base akanda driver
    Abstract driver class that illustrates the minimum expected of methods
    for an akanda driver.
    
    Partially Implements: blueprint appliance-provisioning-driver
    Change-Id: Iaa94a5d5c3adea995e0a581665e67b7396062a00

---
+++ akanda/rug/drivers/base.py
@@ -0,0 +1,87 @@
+# Copyright (c) 2015 AKANDA, INC. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+from oslo_log import log as logging
+
+
+class BaseDriver(object):
+
+    RESOURCE_NAME = 'BaseDriver'
+
+    def __init__(self, worker_context, id, log=None):
+        """This is the abstract for rug drivers.
+
+        :param id: logical resource id
+        :param log: override default log
+        """
+        self.id = id
+        self.external_port = None
+        self.flavor = None
+        self.image_uuid = None
+        self.name = 'ak-%s-%s' % (self.RESOURCE_NAME, self.id)
+
+        if log:
+            self.log = log
+        else:
+            self.log = logging.getLogger(self.name)
+
+        self.post_init(worker_context)
+
+    def post_init(self, worker_context):
+        """post init hook
+
+        :param worker_context:
+        :returns: None
+        """
+        pass
+
+    def pre_boot(self, worker_context):
+        """pre boot hook
+
+        :param worker_context:
+        :returns: None
+        """
+        pass
+
+    def post_boot(self, worker_context):
+        """post boot hook
+
+        :param worker_context:
+        :returns: None
+        """
+        pass
+
+    def update_state(self, worker_context, silent=False):
+        """returns state of logical resource.
+
+        :param worker_context:
+        :param silent:
+        :returns: None
+        """
+        pass
+
+    def build_config(self, worker_context, mgt_port, iface_map):
+        """gets config of logical resource attached to worker_context.
+
+        :param worker_context:
+        :returns: None
+        """
+        pass
+
+    def synchronize_state(self, state):
+        """sometimes a driver will need to update a service behind it with a new
+        state.
+
+        :param state: a valid state
+        """
+        pass
