81bd40cdba62f3f550014b98e05ed9336ff5da3e
David Lenwell <dlenwell@gmail.com>
2015-08-20 09:15:59 -0700

    Driver factory
    Factory for loading drivers.
    
    Partially Implements: blueprint appliance-provisioning-driver
    Change-Id: I1929c4f398a536e1afa01f853a1465d6f3afaaaf

---
+++ akanda/rug/drivers/__init__.py
@@ -0,0 +1,43 @@
+# Copyright (c) 2015 AKANDA, INC. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+from oslo_config import cfg
+
+DRIVER_OPTS = [cfg.ListOpt('available_drivers',
+                           default=list(),
+                           help='a dictionary of the enabled drivers'), ]
+
+cfg.CONF.register_opts(DRIVER_OPTS)
+
+AVAILABLE_DRIVERS = {}
+
+
+class InvalidDriverException(Exception):
+    """Triggered when driver is not available in AVAILABLE_DRIVERS"""
+    pass
+
+
+def get(requested_driver):
+    """Returns driver class based on the requested_driver param
+    will raise InvalidDriverException if not listed in the config option
+    cfg.CONF.available_drivers.
+
+    :param requested_driver: name of desired driver
+    :return: returns driver object
+    """
+    if requested_driver in AVAILABLE_DRIVERS:
+        return AVAILABLE_DRIVERS[requested_driver]
+
+    raise InvalidDriverException(
+        'Failed loading driver: %s' % requested_driver
+    )
