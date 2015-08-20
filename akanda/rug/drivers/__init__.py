f72431f77902c2edd01207d8fae525742404607c
David Lenwell <dlenwell@gmail.com>
2015-08-20 09:15:59 -0700

    Driver factory
    Factory for loading drivers.
    
    Partially Implements: blueprint appliance-provisioning-driver
    Change-Id: I1929c4f398a536e1afa01f853a1465d6f3afaaaf

---
+++ akanda/rug/drivers/__init__.py
@@ -0,0 +1,34 @@
+# Copyright (c) 2015 AKANDA, INC. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+# TODO: (david) make available drivers a config section
+AVAILABLE_DRIVERS = {}
+
+
+class MissingDriverException(Exception):
+    """triggered when driver is not available in available_drivers"""
+    pass
+
+
+def get(requested_driver):
+    """returns driver class based on the name param
+
+    :param name: name of desired driver
+    :return: returns driver object
+    """
+
+    if requested_driver in AVAILABLE_DRIVERS:
+        return AVAILABLE_DRIVERS[requested_driver]
+
+    raise MissingDriverException()
