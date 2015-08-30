42ebeea3b4c0da664f985307a981be63ff5dd2db
David Lenwell <dlenwell@gmail.com>
2015-08-30 16:04:29 -0700

    Router driver and required changes to instance manager (WIP)
    (most passing tests, with adams comments addressed, notifications.py still wonky)
    
    Router driver class that inherits base object and contains all router
    specific code.
    
    NOTE: there are still a few things I need to fix/change before this can
    merge.
    
    Partially Implements: blueprint appliance-provisioning-driver
    Change-Id: I1d2c68b81d28ebb0ca6ce4dcad237c4803bb96d6

--- akanda/rug/resource.py
+++ akanda/rug/resource.py
@@ -1,6 +1,4 @@
-# Copyright 2014 DreamHost, LLC
-#
-# Author: DreamHost, LLC
+# Copyright (c) 2015 AKANDA, INC. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may
 # not use this file except in compliance with the License. You may obtain
@@ -15,20 +13,14 @@
 # under the License.
 
 
-"""Common event format for events passed within the RUG
-"""
+class Resource(object):
+    def __init__(self, driver, _id, _tenant_id):
+        """This is generic resource object
 
-import collections
-
-Event = collections.namedtuple(
-    'Event',
-    ['resource', 'crud', 'body'],
-)
-
-CREATE = 'create'
-READ = 'read'
-UPDATE = 'update'
-DELETE = 'delete'
-POLL = 'poll'
-COMMAND = 'command'  # an external command to be processed
-REBUILD = 'rebuild'
+        :params driver: a string, should match one in drivers.AVAILABLE_DRIVERS
+        :params id: the logical resources id, for a router it would be
+        a neutron router id.
+        """
+        self.driver = driver
+        self.id = _id
+        self.tenant_id = _tenant_id
