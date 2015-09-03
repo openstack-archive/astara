532658b583faba9a7ecd31f492de5abaa9685736
Adam Gandelman <adamg@ubuntu.com>
2015-09-02 18:53:24 -0700

    Enrich functional test suite
    This beefs up the functional test suite to do some tests on a
    tenant router.  The test can now create one-off tenants to be used
    in the tests.  The new tests act entirely on behalf of the test
    router and attempt on only use admin-level things when required.
    
    Change-Id: I26fa70f877522f09075dc87583f2359cc0dcaf41

--- astara/test/functional/test_service_instance.py
+++ astara/test/functional/test_service_instance.py
@@ -1,3 +1,16 @@
+# Copyright (c) 2015 Akanda, Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
 
 from astara.test.functional import base
 
@@ -14,9 +27,10 @@ class AstaraApplianceInstanceTest(base.AstaraFunctionalBase):
         self.assert_router_is_active()
 
     def test_appliance_is_alive(self):
+        router_uuid = self.config['astara_test_router_uuid']
         self.assertTrue(
             self.ak_client.is_alive(
-                host=self.management_address,
+                host=self.get_management_address(router_uuid),
                 port=self.config['appliance_api_port'],
             ),
         )
---
+++ astara/test/functional/test_tenant_router.py
@@ -0,0 +1,81 @@
+# Copyright (c) 2015 Akanda, Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import time
+
+from astara.test.functional import base
+
+
+class TestAstaraRouter(base.AstaraFunctionalBase):
+    @classmethod
+    def setUpClass(cls):
+        super(TestAstaraRouter, cls).setUpClass()
+        cls.tenant = cls.get_tenant()
+        cls.neutronclient = cls.tenant.clients.neutronclient
+
+    def test_get_router_by_creating_a_net(self):
+        """
+        Test to ensure that in a clean tenant, when a network is created,
+        /w an ipv6 subnet, a neutron router and an akanda router will be
+        created as well.
+        """
+        network, router = self.tenant.setup_default_tenant_networking()
+        self.assert_router_is_active(router['id'])
+
+        # refresh router ref now that its active
+        router = self.neutronclient.show_router(router['id'])['router']
+
+        # for each subnet that was created during setup, ensure we have a
+        # router interface added
+        ports = self.neutronclient.list_ports()['ports']
+        subnets = self.neutronclient.list_subnets(network_id=network['id'])
+        subnets = subnets['subnets']
+        self.assertEquals(len(ports), len(subnets))
+        for port in ports:
+            self.assertEquals(port['device_owner'], 'network:router_interface')
+            self.assertEquals(port['device_id'], router['id'])
+            self.assertEquals(
+                sorted([subnet['id'] for subnet in subnets]),
+                sorted([fip['subnet_id'] for fip in port['fixed_ips']])
+            )
+
+        self.ping_router_mgt_address(router['id'])
+
+        # Ensure that if we destroy the nova instance, the RUG will rebuild
+        # the router with a new instance.
+        # This could live in a separate test case but it'd require the
+        # above as setup, so just piggyback on it.
+
+        old_server = self.get_router_appliance_server(router['id'])
+
+        # NOTE(adam_g): In the gate, sometimes the appliance hangs on the
+        # first config update and health checks get queued up behind the
+        # hanging config update.  If thats the case, we need to wait a while
+        # before deletion for the first to timeout.
+        time.sleep(120)
+        self.admin_clients.novaclient.servers.delete(old_server.id)
+
+        # sleep for health_check_period (set by devstack)
+        time.sleep(10)
+
+        # look for the new server, retry giving rug time to do its thing.
+        new_server = self.get_router_appliance_server(
+            router['id'], retries=60, wait_for_active=True)
+        self.assertNotEqual(old_server.id, new_server.id)
+
+        # routers report as ACTIVE initially (LP: #1491673)
+        time.sleep(2)
+
+        self.assert_router_is_active(router['id'])
+        self.ping_router_mgt_address(router['id'])
