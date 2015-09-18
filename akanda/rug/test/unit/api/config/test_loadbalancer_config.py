3c01ec92f547fb92c1bb3253589c49a612d44dc2
Adam Gandelman <adamg@ubuntu.com>
2015-09-18 16:59:21 -0700

    Adds an LBAAS driver
    This is the start of the LBAAS driver.  If you point [lbaas] config
    at the existing akanda appliance image, it should spin up a new instance
    on and manage the neutron resource's state according to the akanda resource.
    
    This LB driver expects to be able to push its config to the akanda-appliance
    server running in the image, and that server is responsible for translating
    what the RUG gives it to something suitable for the LB service installed in
    that image.  There will be a corresponding change to akanda-appliance for this.
    
    The drivers port creation callback currently creates a instance port on the
    same network as the LB VIP and auths it as an allowed adddress pair. During
    config push to the appliance, we include the VIP port in the configs'
    interface map so as to bring up the VIP on the right interface. Note this currently
    does not instruct the appliance to bring up the actual address we create for it.
    
    This requires a corresponding change to akanda-neutron to enable
    the akloadbalancerstatus api extension: http://review.openstack.org/#/c/225370/
    
    And also a change to akanda-appliance: https://review.openstack.org/#/c/226119/
    
    Implements: blueprint lbaas-v2-support
    
    Change-Id: Ic19a883f56fb6d65a83b1f4d93b581f9e242d97f

---
+++ akanda/rug/test/unit/api/config/test_loadbalancer_config.py
@@ -0,0 +1,51 @@
+# Copyright 2014 DreamHost, LLC
+#
+# Author: DreamHost, LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+
+import mock
+
+from akanda.rug.api.config import loadbalancer as lb_config
+from akanda.rug.test.unit import base, fakes
+
+
+class TestLoadbalancerConfigAPI(base.RugTestBase):
+    @mock.patch('akanda.rug.api.config.common.network_config')
+    def test_build_config(self, fake_network_config):
+        fake_client = mock.Mock()
+        fake_lb = fakes.fake_loadbalancer()
+        fake_lb_net = mock.Mock()
+        fake_mgt_net = mock.Mock()
+        fake_mgt_port = mock.Mock(
+            network_id='fake_mgt_network_id',
+        )
+        fake_iface_map = {
+            fake_lb.vip_port.network_id: fake_lb_net,
+            fake_mgt_port.network_id: fake_mgt_net,
+        }
+        fake_network_config.side_effect = [
+            'fake_lb_net_dict', 'fake_mgt_net_dict'
+        ]
+        res = lb_config.build_config(
+            fake_client, fake_lb, fake_mgt_port, fake_iface_map)
+        expected = {
+            'hostname': 'ak-loadbalancer-%s' % fake_lb.tenant_id,
+            'tenant_id': fake_lb.tenant_id,
+            'networks': ['fake_lb_net_dict', 'fake_mgt_net_dict'],
+            'services': {
+                'loadbalancer': fake_lb.to_dict(),
+            }
+        }
+        self.assertEqual(res, expected)
