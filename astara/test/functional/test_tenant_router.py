93261b2a1a6975c4c755a8a36be7d11c3a620181
Adam Gandelman <adamg@ubuntu.com>
2016-03-29 12:29:56 -0700

    devstack: Bump alive_timeout
    Bump alive timeout to 60s to deal with slow qemu instances.
    
    Also, give astara more time to recover from a degraded cluster. This
    can be slow to recover when a slow qemu node is waiting on a long-running
    config update.
    
    Change-Id: I491b96c43d898f7a961725e4f5720af7b9547038

--- astara/test/functional/test_tenant_router.py
+++ astara/test/functional/test_tenant_router.py
@@ -43,8 +43,6 @@ class AstaraRouterTestBase(base.AstaraFunctionalBase):
         router = self.neutronclient.show_router(self.router['id'])
         self.router = router['router']
 
-    HA_ROUTER = False
-
     @property
     def router_ha(self):
         router = self.admin_clients.neutronclient.show_router(
@@ -69,7 +67,7 @@ class TestAstaraHARouter(AstaraRouterTestBase):
         time.sleep(CONF.health_check_period)
 
         service_instances = self.get_router_appliance_server(
-            self.router['id'], ha_router=self.HA_ROUTER)
+            self.router['id'], retries=600, ha_router=self.HA_ROUTER)
         self.assertEqual(len(service_instances), 2)
         self.assertEqual(service_instances[0], backup)
 
