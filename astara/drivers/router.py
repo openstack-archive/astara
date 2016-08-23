3069def8454beafd6e4a11d85baea5650a88a483
Nguyen Hung Phuong <phuongnh@vn.fujitsu.com>
2016-08-23 15:39:50 +0700

    Change order of import in code
    Alphabetically order your imports by the full module path.
    
    http://docs.openstack.org/developer/hacking/#imports
    
    Change-Id: I318fc77bdbcc5e3e0ce158d022568a772b9c084c

--- astara/drivers/router.py
+++ astara/drivers/router.py
@@ -19,14 +19,13 @@ from oslo_utils import timeutils
 
 from neutronclient.common import exceptions as q_exceptions
 
-from astara.common.i18n import _
 from astara.api import astara_client
 from astara.api.config import router as configuration
 from astara import event
 from astara.api import neutron
+from astara.common.i18n import _, _LW
 from astara.drivers.base import BaseDriver
 from astara.drivers import states
-from astara.common.i18n import _LW
 
 LOG = logging.getLogger(__name__)
 
