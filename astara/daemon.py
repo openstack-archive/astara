2afacccf0b60f21e547aac94d56c88e71d1a3dab
Tao Li <litao3721@126.com>
2017-03-21 14:10:30 +0800

    Remove log translations
    Log messages are no longer being translated. This removes all use of the
    _LE, _LI, and _LW translation markers to simplify logging and to avoid
    confusion with new contributions. See:
    http://lists.openstack.org/pipermail/openstack-i18n/2016-November/002574.html
    http://lists.openstack.org/pipermail/openstack-dev/2017-March/113365.html
    
    Change-Id: I5e703f786f6cdf85f2c269e137b9eb671474261d

--- astara/daemon.py
+++ astara/daemon.py
@@ -22,8 +22,6 @@ import signal
 
 from oslo_log import log as logging
 
-from astara.common.i18n import _LI
-
 
 def ignore_signals():
     """Ignore signals that might interrupt processing
@@ -40,5 +38,5 @@ def ignore_signals():
 
     """
     for s in [signal.SIGHUP, signal.SIGUSR1, signal.SIGUSR2, signal.SIGALRM]:
-        logging.getLogger(__name__).info(_LI('ignoring signal %s'), s)
+        logging.getLogger(__name__).info('ignoring signal %s', s)
         signal.signal(s, signal.SIG_IGN)
